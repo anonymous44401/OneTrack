408f7f1cac1c4cb5c1b55e31fd3f74ee1a691896
anonymous44401 <cameronparkinson23@gmail.com>
2024-12-15 19:46:51 +0000

    chore: added scripts

---
+++ src/Static/JS/siteScript.js
@@ -0,0 +1,57 @@
+function mobileMode() {
+    var x = document.getElementById("mobileMenuArea");
+    if (x.style.display === "block") {
+        x.style.display = "none";  
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function updateDepartures() {
+    var input = document.getElementById("searchItem");
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("departuresBox");
+    var tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            var txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+            tr[i].style.display = "none";
+            }
+        }       
+    }
+  }
+
+/* Copy currentl URL */
+function copyText() {
+    /*window.alert(window.location.href)*/
+    navigator.clipboard.writeText(window.location.href);
+}
+
+function mapsClick() {
+    let confirmation = confirm("This link takes you out of OneTrack. Are you sure you want to leave?")
+
+    if (confirmation == true) {
+        open("https://www.opentraintimes.com/")
+    } else {
+        open("http://127.0.0.1:5000/home")
+    }
+}
+
+function confirmSettings() {
+    confirm("Are you sure you want to leave? Any unsaved changes won't be saved.")
+}
+
+/* 
+# TODO Add onclick event to departures to add the clicked item into the search box 
+*/
+/*
+function dimBody() {
+    var element = document.getElementById("pageContent");
+    if (element.style.opacity === "100%")
+        element.style.opacity === "50%"
+}
+*/
